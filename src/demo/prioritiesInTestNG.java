fc19ad845b61b0e4f6d44b9df310440bd626a1e0
sachin221990 <sachinsathe22@gmail.com>
2023-08-23 09:00:35 +0530

    My thrid commit

--- src/demo/prioritiesInTestNG.java
+++ src/demo/prioritiesInTestNG.java
@@ -7,13 +7,13 @@ public class prioritiesInTestNG {
 	//Default priority will be 0 
 	//Priority will be negative number
 	
-	@Test(priority = 12)
+	@Test(priority = 1)
 	public void method1() {
 		System.out.println("prioritiesInTestNGmethod1");
 	}
 	
 	@Test
-	public void method2() {
+	public void method20() {
 		System.out.println("prioritiesInTestNGmethod2");
 	}
 	
